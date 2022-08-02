869a06006646e3813cbb883534d31e1991a8b897
Sidich-S <106704084+Sidich-S@users.noreply.github.com>
2022-08-02 18:56:16 +0300

    2-2

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,7 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
-  };
+    };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
